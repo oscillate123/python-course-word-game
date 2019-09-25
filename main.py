9527df631bf24065002f15e1b1d1cfe344500562
Oscar J Mac <neonode2014@gmail.com>
2019-09-25 20:14:23 +0200

    part 1 finished

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import file_handler as fh
-import funktioner as func
+import game_functions as func
+import game_explaination as game_exp
 
 words_file = "words.txt"
+game_exp.get_game_description(boolean=False)
+word_list = fh.file_reader(read_file=words_file, encoding='ISO-8859-1')  # returns list
+random_word = func.random_list_element(word_list=word_list)
 
 if __name__ == "__main__":
-    x = fh.file_reader(read_file=words_file, encoding='ISO-8859-1')  # returns list
-    y = func.random_word(x)
-    print(y)
+    def main():
+        run = func.Compare(guess=func.guess_input(), word=random_word)
+        run_done = run.compare_words()
+        if not run_done:
+            main()
+
+
+    main()
--- tester.py
+++ tester.py
@@ -1,13 +1,17 @@
-words_list = ["test", "hustak", "hus", "hund", "propp"]
-hints_list = ["t", "h", "u"]
-matches = []
+# words_list = ["test", "hustak", "hus", "hund", "propp"]
+# hints_list = ["t", "h", "u"]
+# matches = []
+#
+#
+# for word in words_list:
+#     for hint in hints_list:
+#         if hint in word:
+#             matches.append(word)
+#         elif hint not in word and word in matches:
+#             matches.remove(word)
+#
+# print(set(matches))
 
+x = "abcde"
 
-for word in words_list:
-    for hint in hints_list:
-        if hint in word:
-            matches.append(word)
-        elif hint not in word and word in matches:
-            matches.remove(word)
-
-print(set(matches))
+print(len(x))
