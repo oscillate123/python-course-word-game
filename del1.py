953f658e0fe5c3cf48d86927019c10683b487a21
Oscar J Mac <neonode2014@gmail.com>
2019-09-25 15:45:03 +0200

    bom

--- del1.py
+++ del1.py
@@ -2,25 +2,32 @@ import read_file as rf
 
 words_db = rf.get_words()
 words_list = words_db.copy()
-hints_list = []
+hints_list = ["a", "t", "r"]
 matches = []
 
 
-def test():
-    print("Vilka 3 bokstäver får ordet innehålla?")
+def checker():
 
-    while len(hints_list) < 3:
-        hints_list.append(input(f"Bokstav {len(hints_list) + 1}:"))
+    for word in words_list:
+        hits = ["_" for _ in word]
+        for idx, char in enumerate(word):
+            for hint in hints_list:
+                if char == hint:
+                    hits[idx] = char
+                    break
 
-        for word in words_list:
-            hits = [0 for _ in word]
-            for idx, char in enumerate(word):
-                for hint in hints_list:
-                    if char == hint:
-                        hits[idx] = 1
+        if len(hints_list) <= sum(hits):
+            matches.append(word)
+        elif len(hints_list) >= sum(hits):
+            words_list.remove(word)
 
-            if len(hints_list) <= sum(hits):
-                matches.append(word)
-            elif len(hints_list) >= sum(hits):
-                words_list.remove(word)
+checker()
+
+
+""" a_tr_ """
+
+""" for word in check_word_list """
+#       if word == check_result
+#            append.word
+#            
 
