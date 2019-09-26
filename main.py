f333eb4ea08f7f7ca7d4e07aca6c00e009c70e19
Oscar J Mac <neonode2014@gmail.com>
2019-09-26 16:18:26 +0200

    updating project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import file_handler as fh
-import game_functions as func
+import game_functions_1 as func_1
+import game_functions_2 as func_2
 import game_explaination as game_exp
 
 if __name__ == "__main__":
@@ -7,7 +8,7 @@ if __name__ == "__main__":
     words_file = "words.txt"
     game_exp.get_game_description(boolean=True)
     word_list = fh.file_reader(read_file=words_file, encoding='ISO-8859-1')  # returns list
-    random_word = func.random_list_element(word_list=word_list)
+    random_word = func_2.random_list_element(word_list=word_list)
 
     def main():
         while True:
@@ -17,8 +18,8 @@ if __name__ == "__main__":
                 pass
 
             if user_input == "2":
-                user_guess = func.guess_input()
-                run = func.Compare(guess=user_guess, word=random_word)
+                user_guess = func_2.guess_input()
+                run = func_2.Compare(guess=user_guess, word=random_word)
                 run_done = run.compare_words()
 
                 if run_done:
