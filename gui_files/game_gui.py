78fd384db983819135ba0ab1227939ab5077d19a
Oscar J Mac <neonode2014@gmail.com>
2019-09-29 00:43:31 +0200

    gui update

--- gui_files/game_gui.py
+++ gui_files/game_gui.py
@@ -1,23 +1,27 @@
 from appJar import gui as gui
 import file_handler as fh
+import game_functions as gf
 
 file_content = fh.file_reader(read_file="words.txt", encoding='ISO-8859-1')
+random_word = gf.random_list_element(file_content)
 
 
 def ordspel_gui():
+    # source: http://appjar.info/*
+
     with gui("Ordspelet") as app:
 
         # BODY SETTINGS
         app.setSize(300, 400)
         app.setLocation(0, 200)
 
-        # Y GROUPS (used for mass controlling column groups)
+        # Y COORDINATE GROUPS (used for mass controlling column groups)
         y_group0 = 0
         y_group1 = 1
         y_group2 = 2
         y_group3 = 3
 
-        # X GROUPS (used for mass controlling row groups)
+        # X COORDINATE GROUPS (used for mass controlling row groups)
         x_group0 = 0
         x_group1 = 1
         x_group2 = 2
@@ -29,27 +33,30 @@ def ordspel_gui():
         app.startTabbedFrame("TabbedFrame")
         app.setTabbedFrameTabExpand("TabbedFrame", expand=True)
 
-        # ------------------------------------- NEW TAB -------------------------------------
+        # ------------------------------------- NEW TAB --------------------------------------
 
         # TAB NAME
         app.startTab("Program vs. User")
 
         app.addLabel("ti_pro_usr", f"saker med grejer", column=0, row=0)
         app.addEmptyLabel("empty_1_0", column=1, row=0)
-        app.addAutoEntry("auto_entry", words=file_content, column=0, row=3)
-        app.setAutoEntryNumRows("auto_entry", 5)
+        app.addAutoEntry("auto_entry", words=file_content, column=0, row=2)
+        app.setAutoEntryNumRows("auto_entry", 3)
 
         app.stopTab()
 
         # ------------------------------------- STOP TAB -------------------------------------
 
-        # ------------------------------------- NEW TAB -------------------------------------
+        # ------------------------------------- NEW TAB --------------------------------------
 
         # TAB NAME
         app.startTab("User vs. Program")
 
         # LABEL COUNTER
         app.label_counter = 1
+
+        # GUESS COUNTER
+        app.guess_counter = 1
 
         def label_picker():
 
@@ -69,8 +76,10 @@ def ordspel_gui():
         # TAB FUNCTIONS
         def press(value):
             if value == "SUBMIT":
+                app.guess_counter += 1
                 # USES Y GROUP 3
-                app.setLabelChan(f"3_EL{label_picker()}", f"{int(app.getEntry('clue'))}")
+                app.setLabel(f"2_L1", f"Guesses: {app.guess_counter}")
+                app.setTextArea("1_L1", end=True, text=f"\n{app.guess_counter} - {random_word}")
 
         # Y GROUP 0
         app.addEmptyLabel("0_EL0", column=y_group0, row=x_group0)
@@ -79,20 +88,27 @@ def ordspel_gui():
         app.addEmptyLabel("0_EL3", column=y_group0, row=x_group3)
 
         # Y GROUP 1 // WORD GUESS HERE AND USER INPUT HERE
-        app.addEmptyLabel("1_EL0", column=y_group1, row=x_group0)
-        app.addLabel("1_L1", f"{app.label_counter}", column=y_group1, row=x_group1)
+        app.addLabel("1_L0", text="Program Guess History:", column=y_group1, row=x_group0)
+
+        app.addScrolledTextArea("1_L1", column=y_group1, row=x_group1)
+        app.setTextArea("1_L1", text=f"{app.guess_counter} - {random_word}")
+
         app.addNumericEntry("clue", column=y_group1, row=x_group2)
         app.addButton("SUBMIT", press, column=y_group1, row=x_group3)
 
         # Y GROUP 2
-        app.addEmptyLabel("2_EL1", column=y_group2, row=x_group0)
+        app.addLabel("2_L1", text="Guesses:", column=y_group2, row=x_group0)
+        app.addEmptyLabel("2_EL1", column=y_group2, row=x_group1)
+        app.addEmptyLabel("2_EL2", column=y_group2, row=x_group2)
+        app.addEmptyLabel("2_EL3", column=y_group2, row=x_group3)
+        app.addEmptyLabel("2_EL4", column=y_group2, row=x_group4)
 
-        # Y GROUP 3 // WORD GUESS HISTORY HERE ; ONLY 4 CONTENT SLOTS
-        app.addLabel("3_EL0", column=y_group3, row=x_group0)
-        app.addEmptyLabel("3_EL1", column=y_group3, row=x_group1)
-        app.addEmptyLabel("3_EL2", column=y_group3, row=x_group2)
-        app.addEmptyLabel("3_EL3", column=y_group3, row=x_group3)
-        app.addEmptyLabel("3_EL4", column=y_group3, row=x_group4)
+        # Y GROUP 3
+        # app.addLabel("3_EL0", text="Historik:", column=y_group3, row=x_group0)
+        # app.addEmptyLabel("3_EL1", column=y_group3, row=x_group1)
+        # app.addEmptyLabel("3_EL2", column=y_group3, row=x_group2)
+        # app.addEmptyLabel("3_EL3", column=y_group3, row=x_group3)
+        # app.addEmptyLabel("3_EL4", column=y_group3, row=x_group4)
 
         app.stopTab()
 
