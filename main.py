0dab16256ffa91ceed6c3359244d37222e6ffa74
Oscar J Mac <neonode2014@gmail.com>
2019-09-28 16:29:06 +0200

    BIG UPDATE V2

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import file_handler as fh
-import game_functions as f
-import ParseUserGuess as PUG
+import game_functions as gf
+import input_functions as input
+import parse_user_guess as pug
 
 if __name__ == "__main__":
 
     WORDS_FILE = "words.txt"
     file_content = fh.file_reader(read_file=WORDS_FILE, encoding='ISO-8859-1')  # returns list
-    # random_word = f.random_list_element(word_list=file_content)
+    # random_word = gf.random_list_element(word_list=file_content)
     random_word = "oscar"
 
-    leader_or_player = f.console_init()
+    leader_or_player = input.console_input()
 
     if leader_or_player == "leader":
         counter_1 = 0
@@ -17,8 +18,8 @@ if __name__ == "__main__":
         flag = True
 
         while flag:
-            r_word = f.random_list_element(word_list)
-            new_list = f.find_related_words(word_list=word_list, robot_guess=r_word, hints=f.leader_input(r_word))
+            r_word = gf.random_list_element(word_list)
+            new_list = gf.find_related_words(word_list=word_list, robot_guess=r_word, hints=input.leader_input(r_word))
             word_list = new_list
             new_list.remove(r_word)
 
@@ -32,8 +33,8 @@ if __name__ == "__main__":
 
         while flag:
             counter_2 += 1
-            user_guess = f.player_input(the_answer=random_word)
-            run = PUG.ParseGuess(guess=user_guess, word=random_word)
+            user_guess = input.player_input(the_answer=random_word)
+            run = pug.ParseGuess(guess=user_guess, word=random_word)
             run_done = run.run_game()
 
             if run_done:
